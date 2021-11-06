09ce601a607411b9c2f678487b5534ec6e8cb0c0
Pablo Escalona Osorio <pa.escalona.o@gmail.com>
2021-11-06 14:06:02 -0300

    CRUD Roles

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -1,8 +1,14 @@
 class Admin::RolesController < ApplicationController
+  
+layout 'admin_layout'
+before_action :set_role, only: [:show, :edit, :update, :delete]
+
   def index
+    @roles = Role.all
   end
 
   def new
+    @role = Role.new
   end
 
   def show
@@ -10,4 +16,39 @@ class Admin::RolesController < ApplicationController
 
   def edit
   end
+
+  def create
+    @role = Role.new(params_role)
+    if @role.save
+      redirect_to admin_roles_path
+    else
+      render :new
+    end
+  end
+  
+  def update
+    if @role.update(params_role)
+      redirect_to admin_roles_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @role.destroy
+    redirect_to admin_roles_path
+  end
+
+  private
+  def params_role
+    params.require(:role).permit(:name)
+  end
+
+  def set_role
+    @role = Role.find(params[:id])
+  rescue
+    flash[:set_role_error] = "Could not find the record #{params[:id]}"
+    redirect_to admin_roles_path
+  end
+
 end
